78efd45ac20aca462b1c6c3715a75e535a513f35
ajogerio <aaogerio@up.edu.ph>
2025-04-17 02:21:31 +0800

    added different icons to each social media

--- src/components/SocialMediaBoxes.jsx
+++ src/components/SocialMediaBoxes.jsx
@@ -4,25 +4,25 @@ import "./SocialMediaBoxes.css";
 export default function SocialMediaBoxes() {
   return (
     <div className="socialMediaBoxesContainer">
+        <Box
+          socialMedia="Instagram"
+          socialMediaIcon={<i className="fa-brands fa-instagram"></i>}
+          socialMediaUsername="@aaogerio"
+        />
       <Box
-        socialMedia="Instagram"
-        socialMediaIcon={<i className="fa-brands fa-instagram"></i>}
-        socialMediaUsername="@aaogerio"
+        socialMedia="Facebook"
+        socialMediaIcon={<i className="fa-brands fa-square-facebook"></i>}
+        socialMediaUsername="@angelo.ogerio"
       />
       <Box
-        socialMedia="Instagram"
-        socialMediaIcon={<i className="fa-brands fa-instagram"></i>}
-        socialMediaUsername="@aaogerio"
+        socialMedia="GitHub"
+        socialMediaIcon={<i className="fa-brands fa-square-github"></i>}
+        socialMediaUsername="@ajogerio"
       />
       <Box
-        socialMedia="Instagram"
-        socialMediaIcon={<i className="fa-brands fa-instagram"></i>}
-        socialMediaUsername="@aaogerio"
-      />
-      <Box
-        socialMedia="Instagram"
-        socialMediaIcon={<i className="fa-brands fa-instagram"></i>}
-        socialMediaUsername="@aaogerio"
+        socialMedia="Email"
+        socialMediaIcon={<i className="fa-solid fa-envelope"></i>}
+        socialMediaUsername="ogerioangelo9@gmail.com"
       />
     </div>
   );
