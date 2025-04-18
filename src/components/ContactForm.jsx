2be17aa158334c572af4c5f31987216ad049944c
ajogerio <aaogerio@up.edu.ph>
2025-04-19 01:17:02 +0800

    added a footer

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,7 +4,7 @@ export default function ContactForm() {
   return (
     <div className="contactFormContainer">
       <h3>Message Form</h3>
-      <p>Send me a message! What do you have in mind? (◕‿◕)</p>
+      <p>What do you have in mind? (◕‿◕)</p>
       <form className="form">
         <div className="formTop">
           <input className="firstName" placeholder="Your First Name" />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,12 @@
+import './Footer.css'
+
 export default function Footer() {
   return( 
-    <>
-    </>
+    <div className="footerContainer">
+      <div className="footerTop">
+      </div>
+      <div className="footerBottom">
+      </div>
+    </div>
   )
 }
